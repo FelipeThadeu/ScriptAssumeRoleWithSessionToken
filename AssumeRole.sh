1495b3594d5798ffe53ee5d3106969d0c6f590e3
Felipe Thadeu <felipet65@gmail.com>
2021-11-12 18:06:28 -0300

    Update script

--- AssumeRole.sh
+++ AssumeRole.sh
@@ -15,17 +15,16 @@ export AWS_ACCESS_KEY_ID=$AccessKeyId
 export AWS_SECRET_ACCESS_KEY=$SecretAccessKey
 export AWS_SESSION_TOKEN=$SessionToken
 
-output2="assume-role-output2.json" # vai gerar um json local
+output="assume-role-output2.json"
 
 read -p "Digite o ID da conta: " AccountID #Vai ler o ID da conta e armazenar na variável
 read -p "Digite a Role ( Dati-acc-access / Dati-PowerUserAccess ): " Role #Vai ler a role e armazenar na variável
 
-echo "$AccountID"
-aws sts assume-role --role-arn arn:aws:iam::$AccountID:role/$Role --role-session-name AWSCLI-Session > $output2
-AccessKeyId2=$(cat $output2 | jq -r '.Credentials''.AccessKeyId')
-SecretAccessKey2=$(cat $output2 | jq -r '.Credentials''.SecretAccessKey')
-SessionToken2=$(cat $output2 | jq -r '.Credentials''.SessionToken')
+aws sts assume-role --role-arn arn:aws:iam::$AccountID:role/$Role --role-session-name AWSCLI-Session > $output
+AccessKeyId=$(cat $output | jq -r '.Credentials''.AccessKeyId')
+SecretAccessKey=$(cat $output | jq -r '.Credentials''.SecretAccessKey')
+SessionToken=$(cat $output | jq -r '.Credentials''.SessionToken')
 
-export AWS_ACCESS_KEY_ID=$AccessKeyId2
-export AWS_SECRET_ACCESS_KEY=$SecretAccessKey2
-export AWS_SESSION_TOKEN=$SessionToken2
+export AWS_ACCESS_KEY_ID=$AccessKeyId
+export AWS_SECRET_ACCESS_KEY=$SecretAccessKey
+export AWS_SESSION_TOKEN=$SessionToken
